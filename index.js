d57ef7465caae94d3fe736b712d21c83a68a5bdd
Jo-gonzalezu <jorgegonzalezuribee@gmail.com>
2022-05-25 22:54:36 -0400

    index.js fixed, file img up.

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ $(document).ready(function() {
 
     $.fn.getPokemon = function(elemento, pokenumber) {
         var pokemon = "";
+        var nombre = "";
         $.ajax({
             url: `https://pokeapi.co/api/v2/pokemon/${pokenumber}`,
 
             success: function(result) {
-                pokemon = result.sprites.front_shiny;
+                pokemon =   result.sprites.front_shiny;
+                nombre  =   result.species.name;
                 elemento.html("<image src=" + pokemon + ">");
+                alert(`¡Un ${nombre} salvaje ha aparecido!`)
             }
         });
     };
 
     $("#pokemon_uno_boton").click(function() {
         $.fn.getPokemon($("#pokemon_uno"), 9);
-        alert("¡Un pokemon salvaje ha aparecido!")
+
     });
 
     $("#pokemon_dos_boton").click(function() {
         $.fn.getPokemon($("#pokemon_dos"), 150);
-        alert("¡Un pokemon salvaje ha aparecido!")
     });
 
     $("#pokemon_tres_boton").click(function() {
         $.fn.getPokemon($("#pokemon_tres"), 383);
-        alert("¡Un pokemon salvaje ha aparecido!")
     });
 });
-
